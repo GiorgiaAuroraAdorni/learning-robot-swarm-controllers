af6f1d6a456f78f5e8529e2737184f0115a83b64
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2020-04-05 13:11:46 +0200

    Refactored code

--- scripts/colour_and_distribute_aligned_myt.py
+++ scripts/colour_and_distribute_aligned_myt.py
@@ -6,105 +6,105 @@ from pid import PID
 
 
 # Superclass: `pyenki.Thymio2` -> the world update step will automatically call the Thymio `controlStep`.
-class ColourDistributedThymio2(pyenki.Thymio2):
+class DistributedThymio2(pyenki.Thymio2):
     INITIAL = 0
     DISTRIBUTING = 1
 
     def __init__(self, name, **kwargs) -> None:
         super().__init__(**kwargs)
         self.name = name
+        # Aseba uses integers and encodes speed between -500 and +500
         self.distributed_controller = PID(-0.01, 0, 0, max_out=500, min_out=-500)
         self.state = self.INITIAL
         self.distribute = True
 
-    def compute_difference(self):
-        # Check if there is a robot ahead using the infrared sensor 2 (front-front)
-        # If the robot is in the range (~14 cm) the maximum front value should be ~1700
+    def neighbors_distance(self):
+        """
+        Check if there is a robot ahead using the infrared sensor 2 (front-front).
+        Check if there is a robot ahead using the infrared sensor 5 (back-left) and 6 (back-right).
+        :return back, front: response values of the rear and front sensors
+        """
         front = self.prox_values[2]
-
-        # Check if there is a robot ahead using the infrared sensor 5 and 6 (back-left) (back-right)
         back = np.mean(np.array([self.prox_values[5], self.prox_values[6]]))
 
-        # Apply correction to the distance measured by the back sensors
+        return back, front
+
+    def compute_difference(self):
+        """
+        :return: the difference between the response value of front and the rear sensor
+        """
+        back, front = self.neighbors_distance()
+
+        # Apply a small correction to the distance measured by the rear sensors: the front sensor used is at a
+        # different x coordinate from the point to which the rear sensor of the robot that follows points. this is
+        # because of the curved shape of the face of the Thymio
         delta_x = 7.41150769
         x = 7.95
-        delta_x_prox_values = 4505 * delta_x / 14
-        x_prox_values = 4505 * x / 14
-        # print('prox', delta_x_prox_values, x_prox_values)
-        correction = x_prox_values - delta_x_prox_values
-        # print('correction ', correction)
-        # print('back and correction', self.name, back, back + correction)
+
+        # Maximum possible response values
+        delta_x_m = 4505 * delta_x / 14
+        x_m = 4505 * x / 14
+
+        correction = x_m - delta_x_m
+
         back += correction
 
         return front - back
 
     def controlStep(self, dt: float) -> None:
         """
-
+        Perform one control step: Move the robots in such a way they stand at equal distances from each other without
+        using communication.
         :param dt: control step duration
         """
-        #  Move the robots in such a way they stand at equal distances from each other without using communication.
-        #  Color the robot half and half they should understand on which side they are compared to the medium.
+
         if self.state == self.INITIAL:
-            # Check if there is a robot ahead using the infrared sensor 2 (front-front)
-            front = self.prox_values[2]
+            back, front = self.neighbors_distance()
 
-            # Check if there is a robot ahead using the infrared sensor 5 and 6 (back-left) (back-right)
-            back = np.mean(np.array([self.prox_values[5], self.prox_values[6]]))
-            print(self.name, self.position, front, back)
-
-            # if self.name in ['myt1', 'myt10']:
+            # Check which are the first and last robots in the line and don't move them
             if front == 0 or back == 0:
                 self.distribute = False
-                self.set_led_top(red=32)
-                # print(self.name, 'blocked')
+                self.set_led_top(red=1.0)
+            else:
+                self.set_led_top(green=1.0)
 
             self.state = self.DISTRIBUTING
 
         elif self.state == self.DISTRIBUTING:
 
             set_point = 0
-            # abs_tollerance = 15
-            front = self.prox_values[2]
-
-            # Check if there is a robot ahead using the infrared sensor 5 and 6 (back-left) (back-right)
-            back = np.mean(np.array([self.prox_values[5], self.prox_values[6]]))
-
-            print(self.name, "F", front, "B", back, "d", self.compute_difference())
+            abs_tollerance = 15
 
             if self.distribute:
-                # if not np.isclose(self.compute_difference(), set_point, atol=abs_tollerance):
-                    # Aseba uses integers and encodes speed between -500 and +500
                 speed = self.distributed_controller.step(self.compute_difference(), dt)
 
-                #     print('Speed', self.name, self.compute_difference(), speed)
-                # else:
-                #     speed = 0
-                #     self.set_led_top(red=32)
-                #     print(self.name, "Finished alignment")
+                if not np.isclose(self.compute_difference(), set_point, atol=abs_tollerance):
+                    self.set_led_top(green=1.0)
+                else:
+                    self.set_led_top(red=1.0)
 
                 self.motor_left_target = speed
                 self.motor_right_target = speed
 
-        # Aseba uses integers and
-        # encodes LED values between 0 and 32 (fully lit)
-        # self.set_led_top(red=32)
-
 
 def setup(aseba: bool = False) -> pyenki.World:
-
+    """
+    Set up the world and create the thymios
+    :param aseba
+    :return world
+    """
     # Create an unbounded world
     world = pyenki.World()
 
     # Create multiple Thymios and position them such as all x-axes are aligned
     myt_quantity = 8
-    myts = [ColourDistributedThymio2(name='myt%d' % (i + 1), use_aseba_units=aseba) for i in range(myt_quantity)]
+    myts = [DistributedThymio2(name='myt%d' % (i + 1), use_aseba_units=aseba) for i in range(myt_quantity)]
 
     # The robots are already arranged in an "indian row" (all x-axes aligned) and within the proximity sensor range
     # ~ 14 cm is the proximity sensors maximal range
-    distances = np.random.randint(2, 12, 8)
+    distances = np.random.randint(5, 8, 8)
 
-    # distance between the origin and the front of the robot along x-axis
+    # Distance between the origin and the front of the robot along x-axis
     constant = 7.95
 
     for i, myt in enumerate(myts):
@@ -113,7 +113,6 @@ def setup(aseba: bool = False) -> pyenki.World:
         else:
             prev_pos = myts[i - 1].position[0]
 
-        # myt.position = (prev_pos + 5 + constant, 0)
         myt.position = (prev_pos + float(distances[i]) + constant, 0)
         myt.angle = 0
         world.add_object(myt)
@@ -122,10 +121,16 @@ def setup(aseba: bool = False) -> pyenki.World:
 
 
 def run(world: pyenki.World, gui: bool = False, T: float = 10, dt: float = 0.1) -> None:
+    """
+    :param world
+    :param gui
+    :param T
+    :param dt: update timestep in seconds, should be below 1 (typically .02-.1)
+    """
 
     if gui:
         # We can either run a simulation [in real-time] inside a Qt application
-        world.run_in_viewer(cam_position=(70, 0), cam_altitude=150.0, cam_yaw=0.0, cam_pitch=-pi / 2)
+        world.run_in_viewer(cam_position=(60, 0), cam_altitude=110.0, cam_yaw=0.0, cam_pitch=-pi / 2)
     else:
         # or we can write our own loop that run the simulation as fast as possible.
         steps = int(T // dt)
