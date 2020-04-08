c3d4d1cf3186ef6505836e25b252c76b78854202
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2020-04-08 12:38:40 +0200

    Refactored code ad saved to file the result of the simulation

--- scripts/generate_simulation_data.py
+++ scripts/generate_simulation_data.py
@@ -1,15 +1,15 @@
-import pyenki
+import os
 import sys
-from math import pi, sqrt
+from csv import writer
+from math import pi, sqrt, sin, cos
+
 import numpy as np
-from pid import PID
+import pyenki
+from utils import check_dir
 
 
 # Superclass: `pyenki.Thymio2` -> the world update step will automatically call the Thymio `controlStep`.
 class DistributedThymio2(pyenki.Thymio2):
-
-    INITIAL = 0
-    DISTRIBUTING = 1
 
     def __init__(self, name, index, goal_position, **kwargs) -> None:
         super().__init__(**kwargs)
@@ -17,42 +17,63 @@ class DistributedThymio2(pyenki.Thymio2):
         self.index = index
         self.goal_position = goal_position
 
-        # Aseba uses integers and encodes speed between -500 and +500
-        self.distributed_controller = PID(-0.01, 0, 0, max_out=500, min_out=-500)
-        self.state = self.INITIAL
-        self.distribute = True
-
-    def euclidean_distance(current_position, goal_postion):
+    def euclidean_distance(self):
         """
-        :param current_position
-        :param goal_postion
         :return: Euclidean distance between current and the goal position
         """
-        return sqrt(pow((goal_postion.position[0] - current_position.position[0]), 2) +
-                    pow((goal_postion.position[1] - current_position.position[1]), 2))
+        return sqrt(pow((self.goal_position[0] - self.position[0]), 2) +
+                    pow((self.goal_position[1] - self.position[1]), 2))
 
-    def linear_vel(current_position, goal_postion, constant=4):
+    def signed_distance(self):
         """
-        :param current_position
-        :param goal_postion
+        :return: Signed distance between current and the goal position, along the current theta of the robot
+        """
+
+        a = self.position[0] * cos(self.angle) + self.position[1] * sin(self.angle)
+        b = self.goal_position[0] * cos(self.angle) + self.goal_position[1] * sin(self.angle)
+
+        return b - a
+
+    def linear_vel(self, constant=4):
+        """
         :param constant
         :return: clipped linear velocity
         """
-        velocity = constant * euclidean_distance(current_position, goal_postion)
+        # velocity = constant * self.euclidean_distance()
+        velocity = constant * self.signed_distance()
         return min(max(-500, velocity), 500)
 
-    def move_to_goal(current_position, goal_postion):
+    def move_to_goal(self):
         """
         Moves the thymio to the goal.
-        :param current_position
-        :param goal_postion
         :return: speed
         """
-        return linear_vel(current_position, goal_postion)
+        return self.linear_vel()
 
+    def controlStep(self, dt: float) -> None:
+        """
+        Perform one control step: Move the robots in such a way they stand at equal distances from each other without
+        using communication.
+        :param dt: control step duration
+        """
+        # FIXME
+        #  sensing prox_comm_events (da cui tiro fuori i mittenti del messaggio e la rispettiva intensità)
+        #   self.prox_comm_tx = self.index
+        #   self.prox_comm_enable = True
+        #   if len(self.prox_comm_events) > 0:
+        #       print()
 
+        speed = self.move_to_goal()
 
+        print(self.name, self.index, self.prox_values, self.position, self.goal_position, speed)
+        self.motor_left_target = speed
+        self.motor_right_target = speed
 
+        line = [self.name, self.index, self.prox_values, self.position, self.goal_position, speed]
+
+        with open(file, 'a+', newline='') as write_obj:
+            csv_writer = writer(write_obj)
+            csv_writer.writerow(line)
 
 
 def setup(aseba: bool = False) -> pyenki.World:
@@ -71,7 +92,7 @@ def setup(aseba: bool = False) -> pyenki.World:
 
     # The robots are already arranged in an "indian row" (all x-axes aligned) and within the proximity sensor range
     # ~ 14 cm is the proximity sensors maximal range
-    distances = np.random.randint(5, 8, 8)
+    distances = np.random.randint(1, 12, 8)
 
     # Distance between the origin and the front of the robot along x-axis
     constant = 7.95
@@ -96,13 +117,19 @@ def setup(aseba: bool = False) -> pyenki.World:
     return world
 
 
-def run(world: pyenki.World, gui: bool = False, T: float = 10, dt: float = 0.1) -> None:
+def run(file, world: pyenki.World, gui: bool = False, T: float = 10, dt: float = 0.1) -> None:
     """
+    :param file
     :param world
     :param gui
     :param T
     :param dt: update timestep in seconds, should be below 1 (typically .02-.1)
     """
+    header = ['name', 'index', 'prox_values', 'position', 'goal_position', 'speed']
+
+    with open(file, 'w', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        csv_writer.writerow(header)
 
     if gui:
         # We can either run a simulation [in real-time] inside a Qt application
@@ -118,7 +145,13 @@ def run(world: pyenki.World, gui: bool = False, T: float = 10, dt: float = 0.1) 
 if __name__ == '__main__':
     world = setup('--aseba' in sys.argv)
 
+    out_dir = 'out/'
+    check_dir(out_dir)
+    s = 4
+
+    file = os.path.join(out_dir, 'simulation-%d.csv' % s)
+
     try:
-        run(world, '--gui' in sys.argv)
+        run(file, world, '--gui' in sys.argv)
     except:
         raise
