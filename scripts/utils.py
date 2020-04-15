46c4de3c9380a8cf1491c8cdc757f8a9eb3d164a
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2020-04-16 01:18:54 +0200

    Added functions to create plots

--- scripts/utils.py
+++ scripts/utils.py
@@ -23,45 +23,83 @@ def dataset_split(file_name, num_run=1000):
     np.save(file_name, x)
 
 
-def plot_setting(train_loss, test_loss, out_dir, model):
-    """
-
-    :param valid_accuracies: OrderedDict containing the model and the correspondent validation accuracy
-    :param out_dir: project directory for the output files
-    :param model_dir: model directory
-    :param epoch: number of epochs of the model
-    :param final: boolean parameter that is True if only if the model is the one selected for the final test
-    """
-    img_dir = out_dir + 'img/'
-    check_dir(img_dir)
-
-    my_plot(train_loss, test_loss, model, img_dir)
-
-
-def my_plot(train_loss, test_loss, model, img_dir):
+def plot_train_test_losses(train_loss, test_loss, img_dir, title, filename, scale=None):
     """
 
     :param train_loss: the training losses
     :param test_loss: the testing losses
-    :param model: name of the model
     :param img_dir: directory for the output image
-    :param title: title of the plot
+    :param title:
+    :param filename:
+    :param scale:
     """
-    title = 'Loss %s' % model
-
     x = np.arange(1, len(train_loss) + 1, dtype=int)
 
     plt.xlabel('epoch', fontsize=11)
     plt.ylabel('loss', fontsize=11)
 
+    plt.xticks(x)
+
     plt.plot(x, train_loss, label='Train ' + title)
     # plt.plot(x, valid_loss, label='Valid ' + title)
     plt.plot(x, test_loss, label='Test ' + title)
-    # plt.ylim(0, 4.5)
+    if scale is not None:
+        plt.ylim(0, scale)
 
     plt.legend()
     plt.title(title, weight='bold', fontsize=12)
 
-    file = os.path.join(img_dir, '%s.png' % title)
+    check_dir(img_dir)
+    file = os.path.join(img_dir, filename)
+    plt.savefig(file)
+    plt.show()
+
+
+def scatter_plot(x, y, x_label, y_label, img_dir, title, filename):
+    """
+     # Plot a scatter plot with the groundtruth on x-axis and prediction on y-axis
+    :param x:
+    :param y:
+    :param x_label:
+    :param y_label:
+    :param img_dir:
+    :param title:
+    :param filename:
+    """
+    plt.xlabel(x_label, fontsize=11)
+    plt.ylabel(y_label, fontsize=11)
+
+    plt.scatter(x, y)
+    plt.title(title, weight='bold', fontsize=12)
+
+    check_dir(img_dir)
+    file = os.path.join(img_dir, filename)
+    plt.savefig(file)
+    plt.show()
+
+
+def plot_prediction_histogram(prediction, x_label, img_dir, title, filename, label=None):
+    """
+     # plot a scatter plot with the groundtruth on x-axis and prediction on y-axis
+    :param prediction:
+    :param x_label:
+    :param img_dir:
+    :param title:
+    :param filename:
+    :param label:
+    """
+    plt.xlabel(x_label, fontsize=11)
+    if label is None:
+        plt.hist(prediction, bins=50)
+    else:
+        plt.style.use('seaborn-deep')
+        plt.hist(prediction, bins=50, label=label)
+        plt.legend()
+    plt.yscale('log')
+
+    plt.title(title, weight='bold', fontsize=12)
+
+    check_dir(img_dir)
+    file = os.path.join(img_dir, filename)
     plt.savefig(file)
     plt.show()
