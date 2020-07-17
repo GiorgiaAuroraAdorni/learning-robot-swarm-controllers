3cb94492943a629c3d9d594991799c639ae6c604
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2020-07-18 00:54:11 +0200

    Added a couple of experiments for task 1 distributed

--- scripts/task1_distributed.sh
+++ scripts/task1_distributed.sh
@@ -12,6 +12,26 @@ python3 source/task1.py --compare-all --net-input prox_values --avg-gap 8 --data
 python3 source/task1.py --controller all --plots-dataset --check-dataset --net-input prox_values --avg-gap 8 --dataset avg_gap-8
 
 
+# Experiment A2
+
+python3 source/task1.py --controller omniscient --generate-dataset --plots-dataset --check-dataset --net-input prox_values --avg-gap 13 --dataset avg_gap-13
+python3 source/task1.py --controller omniscient --generate-split --plots-net --train-net --save-net --net-input prox_values --avg-gap 13 --dataset avg_gap-13 --model net1b
+python3 source/task1.py --controller manual --generate-dataset --plots-dataset --check-dataset --net-input prox_values --avg-gap 13 --dataset avg_gap-13
+python3 source/task1.py --controller learned --generate-dataset --plots-dataset --check-dataset --net-input prox_values --avg-gap 13 --dataset avg_gap-13 --model net1b
+# Only plots for the datasets
+python3 source/task1.py --controller all --plots-dataset --check-dataset --net-input prox_values --avg-gap 13 --dataset avg_gap-13
+
+
+# Experiment A3
+
+python3 source/task1.py --controller omniscient --generate-dataset --plots-dataset --check-dataset --net-input prox_values --avg-gap 25 --dataset avg_gap-25
+python3 source/task1.py --controller omniscient --generate-split --plots-net --train-net --save-net --net-input prox_values --avg-gap 25 --dataset avg_gap-25 --model net1c
+python3 source/task1.py --controller manual --generate-dataset --plots-dataset --check-dataset --net-input prox_values --avg-gap 25 --dataset avg_gap-25
+python3 source/task1.py --controller learned --generate-dataset --plots-dataset --check-dataset --net-input prox_values --avg-gap 25 --dataset avg_gap-25 --model net1c
+# Only plots for the datasets
+python3 source/task1.py --controller all --plots-dataset --check-dataset --net-input prox_values --avg-gap 25 --dataset avg_gap-25
+
+
 # Experiment B
 
 python3 source/task1.py --controller omniscient --generate-dataset --plots-dataset --check-dataset --net-input prox_comm --avg-gap 8 --dataset avg_gap-8
