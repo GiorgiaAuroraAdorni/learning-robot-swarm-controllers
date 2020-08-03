bc2809f77c9fbd1de75fa109820f1d887b14ade2
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2020-08-03 15:53:48 +0200

    Updated sensor to distances and generated plot of the response

--- source/utils/sensors_to_distances.py
+++ source/utils/sensors_to_distances.py
@@ -1,6 +1,12 @@
+import os
+import pickle
+
+import matplotlib.pyplot as plt
+import my_plots
 import numpy as np
 import pyenki
-from utils import get_prox_comm, get_all_sensors
+
+import utils
 
 
 class Thymio(pyenki.Thymio2):
@@ -34,11 +40,11 @@ class Thymio(pyenki.Thymio2):
         if len(self.prox_comm_events) == 0:
             prox_comm = {'sender': {'intensities': [0, 0, 0, 0, 0, 0, 0]}}
         else:
-            prox_comm = get_prox_comm(self)
+            prox_comm = utils.get_prox_comm(self)
 
         prox_values = getattr(self, 'prox_values').copy()
 
-        sensing = get_all_sensors(prox_values, prox_comm)
+        sensing = utils.get_all_sensors(prox_values, prox_comm)
 
         return sensing
 
@@ -53,10 +59,13 @@ class Thymio(pyenki.Thymio2):
         self.prox_comm_tx = 0
         self.prox_comm_enable = True
 
-distances = np.arange(51)
+distances = np.arange(48)
+front_prox_values = np.zeros(48)
+back_prox_values = np.zeros(48)
+front_prox_comms = np.zeros(48)
+back_prox_comms = np.zeros(48)
 
-for distance in distances:
-
+for idx, distance in enumerate(distances):
 
     myt_quantity = 3
     # min_distance = 10.9
@@ -96,17 +105,21 @@ for distance in distances:
         world.step(dt=0.1)
 
         sensing = myts[1].get_input_sensing()
-        front_prox_values = sensing[2]
-        back_prox_values = np.mean([sensing[5], sensing[6]])
+        front_prox_value = sensing[2]
+        back_prox_value = np.mean([sensing[5], sensing[6]])
 
         front_prox_comm = sensing[9]
         back_prox_comm = np.mean([sensing[12], sensing[13]])
 
-        a.append(int(front_prox_values))
-        b.append(int(back_prox_values))
+        a.append(int(front_prox_value))
+        b.append(int(back_prox_value))
         c.append(int(front_prox_comm))
         d.append(int(back_prox_comm))
 
+    front_prox_values[idx] = int(np.mean(a))
+    back_prox_values[idx] = int(np.mean(b))
+    front_prox_comms[idx] = int(np.mean(c))
+    back_prox_comms[idx] = int(np.mean(d))
 
     sensors['front_prox_values'] = int(np.mean(a))
     sensors['back_prox_values'] = int(np.mean(b))
@@ -115,5 +128,29 @@ for distance in distances:
 
     print(sensors)
 
-    # world.run_in_viewer(cam_position=(10.9, 0), cam_altitude=50.0, cam_yaw=0.0, cam_pitch=-np.pi / 2,
-    #                     walls_height=10.0, orthographic=True, period=0.1)
+# front_prox_values[front_prox_values == 0] = np.nan
+# back_prox_values[back_prox_values == 0] = np.nan
+# front_prox_comms[front_prox_comms == 0] = np.nan
+# back_prox_comms[back_prox_comms == 0] = np.nan
+
+sensing_to_distances = [distances, front_prox_values, back_prox_values, front_prox_comms, back_prox_comms]
+
+file = os.path.join('controllers', 'sensing_to_distances.pkl')
+with open(file, 'wb') as f:
+    pickle.dump(sensing_to_distances, f)
+
+
+plt.figure()
+plt.xlabel('distance', fontsize=11)
+plt.ylabel('sensing', fontsize=11)
+
+plt.plot(distances, front_prox_values, label='front_prox_values')
+plt.plot(distances, back_prox_values,  label='back_prox_values')
+plt.plot(distances, front_prox_comms,  label='front_prox_comm')
+plt.plot(distances, back_prox_comms,   label='back_prox_comm')
+plt.legend()
+
+my_plots.save_visualisation('sensing_to_distances', 'controllers')
+
+# world.run_in_viewer(cam_position=(10.9, 0), cam_altitude=50.0, cam_yaw=0.0, cam_pitch=-np.pi / 2,
+#                     walls_height=10.0, orthographic=True, period=0.1)
