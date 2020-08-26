247821dcd4a64e027de5e8ecec82072530a57fa6
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2020-08-26 10:52:01 +0200

    Updated task2_communication script and converted to an executable

--- scripts/task2_communication.sh
+++ scripts/task2_communication.sh
@@ -1,5 +1,6 @@
 # Experiment A
 
+# Generate omniscient datasets and plots
 python3 source/task2.py --dataset avg_gap-8-N-5 --model net-v1 --myt-quantity 5 --avg-gap 8 --controller omniscient --generate-dataset --plots-dataset
 python3 source/task2.py --dataset avg_gap-20-N-5 --model net-v2 --avg-gap 20 --myt-quantity 5 --controller omniscient --generate-dataset --plots-dataset
 python3 source/task2.py --dataset avg_gap-8-N-8 --model net-v3 --myt-quantity 8 --avg-gap 8 --controller omniscient --generate-dataset --plots-dataset
@@ -10,8 +11,18 @@ python3 source/task2.py --dataset avg_gap-8-N-variable --model net-v7 --avg-gap 
 python3 source/task2.py --dataset avg_gap-20-N-variable --model net-v8 --avg-gap 20 --controller omniscient --generate-dataset --plots-dataset
 python3 source/task2.py --dataset avg_gap-variable-N-variable --model net-v9 --controller omniscient --generate-dataset --plots-dataset
 
+# Generate manual datasets and plots
+python3 source/task2.py --dataset avg_gap-8-N-5 --model net-v1 --myt-quantity 5 --avg-gap 8 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-20-N-5 --model net-v2 --avg-gap 20 --myt-quantity 5 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-8-N-8 --model net-v3 --myt-quantity 8 --avg-gap 8 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-20-N-8 --model net-v4 --avg-gap 20 --myt-quantity 8 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-variable-N-5 --model net-v5 --myt-quantity 5 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-variable-N-8 --model net-v6 --myt-quantity 8 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-8-N-variable --model net-v7 --avg-gap 8 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-20-N-variable --model net-v8 --avg-gap 20 --controller manual --generate-dataset --plots-dataset
+python3 source/task2.py --dataset avg_gap-variable-N-variable --model net-v9 --controller manual --generate-dataset --plots-dataset
 
-
+# Train nets and generate plots
 python3 source/task2.py --dataset avg_gap-8-N-5 --model net-v1 --myt-quantity 5 --avg-gap 8 --controller omniscient --generate-split --train-net --plots-net
 python3 source/task2.py --dataset avg_gap-20-N-5 --model net-v2 --avg-gap 20 --myt-quantity 5 --controller omniscient --generate-split --train-net --plots-net
 python3 source/task2.py --dataset avg_gap-8-N-8 --model net-v3 --myt-quantity 8 --avg-gap 8 --controller omniscient --generate-split --train-net --plots-net
@@ -21,4 +32,3 @@ python3 source/task2.py --dataset avg_gap-variable-N-8 --model net-v6 --myt-quan
 python3 source/task2.py --dataset avg_gap-8-N-variable --model net-v7 --avg-gap 8 --controller omniscient --generate-split --train-net --plots-net
 python3 source/task2.py --dataset avg_gap-20-N-variable --model net-v8 --avg-gap 20 --controller omniscient --generate-split --train-net --plots-net
 python3 source/task2.py --dataset avg_gap-variable-N-variable --model net-v9 --controller omniscient --generate-split --train-net --plots-net
-
